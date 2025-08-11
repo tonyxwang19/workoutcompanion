6454a85756ed0c56f66fc6ce21e5e1e5d5de9402
xwang19 <58502876+tonyxwang19@users.noreply.github.com>
2025-08-11 10:14:48 +0800

    Initial Commit

---
+++ WorkoutCompanionLLM/WorkoutCompanionLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutCompanionLLMApp.swift
+//  WorkoutCompanionLLM
+//
+//  Created by 王希宁的Macbook on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutCompanionLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutCompanionLLMTests/WorkoutCompanionLLMTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutCompanionLLMTests.swift
+//  WorkoutCompanionLLMTests
+//
+//  Created by 王希宁的Macbook on 8/11/25.
+//
+
+import Testing
+@testable import WorkoutCompanionLLM
+
+struct WorkoutCompanionLLMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
